d224678c479587204ddeeb33e9199b55941eaf03
josh-hensley <me@joshhensley.com>
2024-11-11 19:17:37 -0600

    created nav page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <ul className="nav">
+        <li className="nav-item"><a href="/" className="nav-link">Candidate Search</a></li>
+        <li className="nav-item"><a href="/SavedCandidates" className="nav-link">Saved Candidates</a></li>
+      </ul>
+    </nav>
   )
 };
 
