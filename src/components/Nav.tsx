26da03e8618d64ca2e7e5b8bae96a82c347c1255
josh-hensley <me@joshhensley.com>
2024-11-11 20:26:49 -0600

    working on implementing candidate search, and figuring out useEffect NOT WORKING

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,4 @@
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
     <nav>
       <ul className="nav">
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,69 @@
-// import { useState, useEffect } from 'react';
-// import { searchGithub, searchGithubUser } from '../api/API';
+import { useState, useEffect } from 'react';
+import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  let index = 0;
+  const getUsers =  async () => {
+    return await searchGithub() as string[];
+  }
+  const users: Promise<string[]> = getUsers();
+  useEffect(()=>{
+  })
+  const [ currentCandidate, setCandidate ] = useState<Candidate>({
+    name: "",
+    username: "", 
+    location: "",
+    avatar: "", 
+    email: "", 
+    html_url: "",
+    company: ""
+  });
+
+  
+  const handleClickMinus = async () => {
+    index--;
+    const { name, username, location, avatar, email, html_url, company } = await searchGithubUser(users[index]);
+    setCandidate({
+      name,
+      username,
+      location,
+      avatar,
+      email,
+      html_url,
+      company
+    })
+  }
+
+  const handleClickPlus = async () => {
+    index++;
+    const { name, username, location, avatar, email, html_url, company } = await searchGithubUser(users[index]);
+    setCandidate({
+      name,
+      username,
+      location,
+      avatar,
+      email,
+      html_url,
+      company
+    })
+  }
+
+  return (
+    <>
+      <h1>CandidateSearch</h1>
+      <h3>{ currentCandidate.name }</h3>
+      <img src={ currentCandidate.avatar } alt="" />
+      <p>{ currentCandidate.username }</p>
+      <p>{ currentCandidate.location }</p>
+      <p>{ currentCandidate.email }</p>
+      <p>{ currentCandidate.html_url }</p>
+      <p>{ currentCandidate.company }</p>
+      <button type="button" onClick={handleClickMinus}>-</button>
+      <button type="button" onClick={handleClickPlus}>+</button>
+    </>
+  );
 };
+
 
 export default CandidateSearch;
