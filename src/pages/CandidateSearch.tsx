238c7c76b6599b86171a9c94d236ca3e2216d1cf
josh-hensley <me@joshhensley.com>
2024-11-12 18:33:49 -0600

    fixed env bug

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,58 +1,56 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
-import Candidate from '../interfaces/Candidate.interface';
+// import { useState } from 'react';
+import { searchGithub } from '../api/API';
+// import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-  let index = 0;
-  const getUsers =  async () => {
+  const getUsers = async ()=>{
     return await searchGithub() as string[];
   }
-  const users: Promise<string[]> = getUsers();
-  useEffect(()=>{
-  })
-  const [ currentCandidate, setCandidate ] = useState<Candidate>({
-    name: "",
-    username: "", 
-    location: "",
-    avatar: "", 
-    email: "", 
-    html_url: "",
-    company: ""
-  });
-
+  const users = getUsers();
+  console.log(users);
+  // let index: number = 0;
+  // const [ currentCandidate, setCandidate ] = useState<Candidate>({
+  //   name: "",
+  //   username: "", 
+  //   location: "",
+  //   avatar: "", 
+  //   email: "", 
+  //   html_url: "",
+  //   company: ""
+  // });
   
-  const handleClickMinus = async () => {
-    index--;
-    const { name, username, location, avatar, email, html_url, company } = await searchGithubUser(users[index]);
-    setCandidate({
-      name,
-      username,
-      location,
-      avatar,
-      email,
-      html_url,
-      company
-    })
-  }
+  // const handleClickMinus = async () => {
+  //   index--;
+  //   const { name, username, location, avatar, email, html_url, company } = await searchGithubUser();
+  //   setCandidate({
+  //     name,
+  //     username,
+  //     location,
+  //     avatar,
+  //     email,
+  //     html_url,
+  //     company
+  //   })
+  // }
 
-  const handleClickPlus = async () => {
-    index++;
-    const { name, username, location, avatar, email, html_url, company } = await searchGithubUser(users[index]);
-    setCandidate({
-      name,
-      username,
-      location,
-      avatar,
-      email,
-      html_url,
-      company
-    })
-  }
+  // const handleClickPlus = async () => {
+  //   index++;
+  //   const { name, username, location, avatar, email, html_url, company } = await searchGithubUser(users[index]);
+  //   setCandidate({
+  //     name,
+  //     username,
+  //     location,
+  //     avatar,
+  //     email,
+  //     html_url,
+  //     company
+  //   })
+  // }
 
   return (
     <>
       <h1>CandidateSearch</h1>
-      <h3>{ currentCandidate.name }</h3>
+      {/* <h3>{ currentCandidate.name }</h3>
       <img src={ currentCandidate.avatar } alt="" />
       <p>{ currentCandidate.username }</p>
       <p>{ currentCandidate.location }</p>
@@ -60,7 +58,7 @@ const CandidateSearch = () => {
       <p>{ currentCandidate.html_url }</p>
       <p>{ currentCandidate.company }</p>
       <button type="button" onClick={handleClickMinus}>-</button>
-      <button type="button" onClick={handleClickPlus}>+</button>
+      <button type="button" onClick={handleClickPlus}>+</button> */}
     </>
   );
 };
